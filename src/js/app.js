9200f0b6e263e91d1b2a99173b5ea7c22eb09934
Nico4000 <nicoproject@ya.ru>
2021-03-29 20:40:17 +0300

    add masked input

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,10 @@
-import '../scss/app.scss';
+/* JQuery plugins */
+require('jquery-mask-plugin')
+
+import '../scss/app.scss'
 
 /* Components */
 import './components/dropdown'
+
+/* UI Kit */
+$('.masked-input').mask('00/00/0000', { placeholder: 'ДД.ММ.ГГГГ' })
