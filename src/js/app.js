4ac9886c5167ff77e1fe0045c588a6294a53afcb
Nico4000 <nicoproject@ya.ru>
2021-04-05 18:01:04 +0300

    add room-details, add chart.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ require('air-datepicker')
 require('jquery-mask-plugin')
 require('ion-rangeslider')
 require('../../node_modules/bxslider/src/js/jquery.bxslider.js')
+import Chart from 'chart.js/auto';
 
 // import 'bxslider'
 
@@ -48,10 +49,51 @@ $('.js-range-slider').ionRangeSlider({
   grid: false,
   skin: 'round',
   postfix: "<span class='rub'>Р</span>",
-  hide_min_max: true, 
+  hide_min_max: true,
 })
 
 $('.bxslider').bxSlider({
   nextText: '<i class="material-icons">keyboard_arrow_right</i>',
   prevText: '<i class="material-icons">keyboard_arrow_left</i>',
 })
+
+//Drawing Chart for Room Details Page
+const el = document.getElementById('myChart')
+const gradientOrange = el.getContext('2d').createLinearGradient(0, 0, 0, 100);
+const gradientPurple = el.getContext('2d').createLinearGradient(0, 0, 0, 100);
+const gradientGreen = el.getContext('2d').createLinearGradient(0, 0, 0, 100);
+
+gradientOrange.addColorStop(0, '#FFE39C');
+gradientOrange.addColorStop(1, '#FFBA9C ');
+
+gradientPurple.addColorStop(0, '#BC9CFF');
+gradientPurple.addColorStop(1, '#8BA4F9');
+
+gradientGreen.addColorStop(0, ' #6FCF99');
+gradientGreen.addColorStop(1, '#6BD0BE');
+
+const chartConfig = {
+      type: 'doughnut',
+      data: {
+        datasets: [
+          {
+            data: [5, 5, 10, 0],
+            backgroundColor: [gradientPurple, gradientGreen, gradientOrange],
+            borderWidth: 2,
+          },
+        ],
+        labels: ['Удовлетворительно', 'Хорошо', 'Великолепно'],
+      },
+      options: {
+        legend: {
+          display: false,
+        },
+        maintainAspectRatio: false,
+        cutoutPercentage: 10,
+      },
+}
+
+window.onload = function () {
+  const ctx = document.getElementById('myChart').getContext('2d')
+  new Chart(ctx, chartConfig)
+}
