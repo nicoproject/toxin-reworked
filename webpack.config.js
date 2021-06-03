e32584139f764a31978ae8c4d4cac4df95f56966
Nico4000 <nicoproject@ya.ru>
2021-06-03 16:19:38 +0300

    change file structure (not completed)

--- webpack.config.js
+++ webpack.config.js
@@ -14,111 +14,111 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const { extendDefaultPlugins } = require('svgo')
 
 /* ENV const */
-const environment = require('./configuration/environment')
-const PAGES_DIR = `${environment.paths.source}\\pug\\pages\\`
+const environment = require('./config/environment')
+const PAGES_DIR = `${environment.paths.source}\\pages\\`
 const PAGES = fs
-  .readdirSync(PAGES_DIR)
-  .filter((fileName) => fileName.endsWith('.pug'))
+	.readdirSync(PAGES_DIR)
+	.filter((fileName) => fileName.endsWith('.pug'))
 
 module.exports = {
-  entry: {
-    app: path.resolve(environment.paths.source, 'js', 'app.js'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.pug$/,
-        use: ['pug-loader'],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                config: path.resolve('./postcss.config.js'),
-              },
-              sourceMap: true,
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].css',
-    }),
-    new ImageMinimizerPlugin({
-      test: /\.(jpe?g|png|gif|svg)$/i,
-      minimizerOptions: {
-        // Lossless optimization with custom option
-        // Feel free to experiment with options for better result for you
-        plugins: [
-          ['gifsicle', { interlaced: true }],
-          ['jpegtran', { progressive: true }],
-          ['optipng', { optimizationLevel: 5 }],
-          [
-            'svgo',
-            {
-              plugins: extendDefaultPlugins([
-                {
-                  name: 'removeViewBox',
-                  active: false,
-                },
-              ]),
-            },
-          ],
-        ],
-      },
-    }),
-    new CleanWebpackPlugin({
-      verbose: true,
-      cleanOnceBeforeBuildPatterns: ['**/*', '!stats.json'],
-    }),
-    ...PAGES.map(
-      (page) =>
-        new HTMLWebpackPlugin({
-          template: `${PAGES_DIR}/${page}`,
-          filename: `./${page.replace(/\.pug/, '.html')}`,
-        }),
-    ),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.resolve(environment.paths.source, 'images'),
-          to: path.resolve(environment.paths.output, 'images'),
-          toType: 'dir',
-          globOptions: {
-            ignore: ['*.DS_Store', 'Thumbs.db'],
-          },
-        },
-      ],
-    }),
-  ],
-  // ].concat(htmlPluginEntries),
-  target: 'web',
+	entry: {
+		app: path.resolve(environment.paths.source, 'js', 'app.js'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.pug$/,
+				use: ['pug-loader'],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true,
+						},
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: {
+								config: path.resolve('./postcss.config.js'),
+							},
+							sourceMap: true,
+						},
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+		],
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery',
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].css',
+		}),
+		new ImageMinimizerPlugin({
+			test: /\.(jpe?g|png|gif|svg)$/i,
+			minimizerOptions: {
+				// Lossless optimization with custom option
+				// Feel free to experiment with options for better result for you
+				plugins: [
+					['gifsicle', { interlaced: true }],
+					['jpegtran', { progressive: true }],
+					['optipng', { optimizationLevel: 5 }],
+					[
+						'svgo',
+						{
+							plugins: extendDefaultPlugins([
+								{
+									name: 'removeViewBox',
+									active: false,
+								},
+							]),
+						},
+					],
+				],
+			},
+		}),
+		new CleanWebpackPlugin({
+			verbose: true,
+			cleanOnceBeforeBuildPatterns: ['**/*', '!stats.json'],
+		}),
+		...PAGES.map(
+			(page) =>
+				new HTMLWebpackPlugin({
+					template: `${PAGES_DIR}/${page}`,
+					filename: `./${page.replace(/\.pug/, '.html')}`,
+				}),
+		),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(environment.paths.source, 'assets/images'),
+					to: path.resolve(environment.paths.output, 'assets/images'),
+					toType: 'dir',
+					globOptions: {
+						ignore: ['*.DS_Store', 'Thumbs.db'],
+					},
+				},
+			],
+		}),
+	],
+	// ].concat(htmlPluginEntries),
+	target: 'web',
 }
